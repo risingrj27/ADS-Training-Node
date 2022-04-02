eb22a365baf7dd8d26f61210a4b34e86db7fbac7
risingrj27 <rjprince18@gmail.com>
2022-04-03 00:29:24 +0530

    02-04-2022

---
+++ 02-04-2022/server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); 
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(DB,{
+  useNewUrlParser:true,
+  // useCreateIndex:true,
+  // useFindAndModify:false
+}).then(con=>{ console.log("DB connection successfull")
+})
+// console.log(process.env.NODE_ENV);
+// console.log(app.get('production'))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your server is running ${port}`);
+});
